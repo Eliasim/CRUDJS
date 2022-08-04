9bb9efca3b635282fc997b65e8383f4b0f14f82a
Eliasim Mendoza <eliasimmzsa@gmail.com>
2022-08-04 14:33:44 -0500

    Cambio en el diseño de body mas inicio del codigo en JS

---
+++ javaCRUD.js
@@ -0,0 +1,9 @@
+function guardar(){
+    var nombre = document.getElementById("Nombre").value;
+    var fecha = document.getElementById("Fecha").value;
+    var telefono = document.getElementById("Telefono").value;
+    var edad = document.getElementById("Edad").value;
+    var trabajo = document.getElementById("Trabajo").value;
+    
+    console.log(nombre, fecha, telefono, edad, trabajo);
+}
