d89c4f39ec1d3d40fd1fc19d062eef6e22722d22
Eliasim Mendoza <eliasimmzsa@gmail.com>
2022-08-08 13:37:08 -0500

    El CRUD ya guarda los datos en un array y se crearon los botones para eliminar y editar

--- javaCRUD.js
+++ javaCRUD.js
@@ -1,9 +1,35 @@
+var listaPersonas = [];
+
 function guardar(){
-    var nombre = document.getElementById("Nombre").value;
-    var fecha = document.getElementById("Fecha").value;
-    var telefono = document.getElementById("Telefono").value;
-    var edad = document.getElementById("Edad").value;
-    var trabajo = document.getElementById("Trabajo").value;
+    var gNombre = document.getElementById("inNombre").value;
+    var gFecha = document.getElementById("inFecha").value;
+    var gTelefono = document.getElementById("inTelefono").value;
+    var gEdad = document.getElementById("inEdad").value;
+    var gTrabajo = document.getElementById("inTrabajo").value;
     
-    console.log(nombre, fecha, telefono, edad, trabajo);
+    
+    listaPersonas = [...listaPersonas,{gNombre, gFecha, gTelefono, gEdad, gTrabajo}];    
+    mostrar();    
 }
+
+function mostrar(){
+    var node = document.createElement("li");
+    var btnEditar = document.createElement("button");
+    var btnEliminar = document.createElement("button");
+    
+    btnEditar.textContent = "Editar";
+    btnEliminar.textContent = "Eliminar";
+
+    listaPersonas.forEach(element => {
+        node.appendChild(document.createTextNode(element.gNombre + " / " + element.gFecha+ " / " + element.gTelefono+ " / " + element.gEdad+ " / " + element.gTrabajo));
+        document.querySelector("ul").appendChild(node);
+        document.querySelector("ul").appendChild(btnEditar);
+        btnEditar.addEventListener('click',function eliminar(){
+            //listaPersonas.splice(element,1);
+            console.log(element);
+        });
+        document.querySelector("ul").appendChild(btnEliminar);
+        listaPersonas = [];
+    });
+}
+
